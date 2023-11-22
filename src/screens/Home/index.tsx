f887e5c15097a51f7c0768905470e405fab5e8b7
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-11-22 19:12:58 -0300

    criando input e button de adicionar tarefa

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,25 @@
-import { Image, Text, View } from "react-native";
-import { styles } from "./styles";
+import React from 'react';
+import { Image, TextInput, TouchableOpacity, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { styles } from './styles';
 
-export function Home(){
-    return(
-        <View style={styles.container}>
-            <View style={styles.header}>
-            <Image 
-            style={styles.logo}
-            source={require('../../assets/Logo.png')} />
-
-                
-            </View>
-             <Text>ToDo List</Text>
-             
-        </View>
-    )
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image style={styles.logo} source={require('../../assets/Logo.png')} />
+      </View>
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor="#808080"
+          style={styles.textInput}
+        />
+        <TouchableOpacity style={styles.button}>
+          {/* Utilizando o ícone corretamente */}
+          <Icon name="plus-circle" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -14,5 +14,36 @@ export const styles = StyleSheet.create({
     },
     logo:{
         height:30,  
+    },
+    form:{
+        width:'100%',
+        padding:24,
+        flexDirection:'row',
+        position:'absolute',
+        marginTop:150,
+    },
+    textInput:{
+        flex:1,
+        height:54,
+        backgroundColor:'#262626',
+        borderWidth:1,
+        borderColor: '#0D0D0D',
+        borderRadius:5,
+        color:'#FFFFFF',
+        padding: 16,
+        fontSize: 16,
+        marginRight: 4,
+
+    },
+    button:{
+        height:54,
+        width:54,
+        backgroundColor:'#1E6F9F',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:5,
+    },
+    image:{
+
     }
 })
