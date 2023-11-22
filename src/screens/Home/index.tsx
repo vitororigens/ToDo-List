64f8715b19579fc72cea666fcdf30da6ae5b599c
DevVitorPlay <vitor.abreu@playmovel.com.br>
2023-11-22 15:20:06 -0300

    fetch:header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { styles } from "./styles";
 
 export function Home(){
     return(
-        <View>
+        <View style={styles.container}>
+            <View style={styles.header}>
+            <Image 
+            style={styles.logo}
+            source={require('../../assets/Logo.png')} />
+
+                
+            </View>
              <Text>ToDo List</Text>
+             
         </View>
     )
 }
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#808080',
+    },
+    header:{
+        backgroundColor:'#0D0D0D',
+        height:200,
+        width:'100%',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    logo:{
+        height:30,  
+    }
+})
